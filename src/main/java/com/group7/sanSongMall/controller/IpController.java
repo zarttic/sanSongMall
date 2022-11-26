36c03f479875af9d02712e497841f4ac41ac3847
liyajun <332209078@qq.com>
2022-11-27 01:49:20 +0800

    查询ip地址

---
+++ src/main/java/com/group7/sanSongMall/controller/IpController.java
@@ -0,0 +1,37 @@
+/**
+ * {@code @Description}
+ *
+ * @author liyajun
+ * {@code @create}          2022-11-26 18:28
+ */
+
+package com.group7.sanSongMall.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/ip")
+public class IpController {
+    @GetMapping("/ip-address")
+    public static String getIpAddress(HttpServletRequest request) throws IOException {
+        // 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip.length() > 15) {
+            String[] ips = ip.split(",");
+            for (int index = 0; index < ips.length; index++) {
+                String strIp = (String) ips[index];
+                if (!("unknown".equalsIgnoreCase(strIp))) {
+                    ip = strIp;
+                    break;
+                }
+            }
+        }
+        return ip;
+    }
+}
+
